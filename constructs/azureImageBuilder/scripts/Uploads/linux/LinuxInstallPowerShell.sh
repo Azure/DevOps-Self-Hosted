8f55d6a51c385fe83193d9d450f61fd3e5817d98
MrMCake <asehr@hotmail.de>
2022-06-20 21:16:04 +0200

    Changed PS install path

--- constructs/azureImageBuilder/scripts/Uploads/linux/LinuxInstallPowerShell.sh
+++ constructs/azureImageBuilder/scripts/Uploads/linux/LinuxInstallPowerShell.sh
@@ -1,5 +1,5 @@
 # Download the Microsoft repository GPG keys
-wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
+wget -q https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb
 
 # Register the Microsoft repository GPG keys
 sudo dpkg -i packages-microsoft-prod.deb
