e01680f9c02aa1e9c7d6c901fcdae324c15dff73
AlexanderSehr <asehr@hotmail.de>
2023-12-30 19:14:46 +0100

    Updated install file

--- constructs/azureImageBuilder/scripts/Uploads/linux/LinuxInstallPowerShell.sh
+++ constructs/azureImageBuilder/scripts/Uploads/linux/LinuxInstallPowerShell.sh
@@ -1,8 +1,22 @@
+# Source: https://learn.microsoft.com/en-us/powershell/scripting/install/install-ubuntu?view=powershell-7.4
+
+# Update the list of packages
+sudo apt-get update
+
+# Install pre-requisite packages.
+sudo apt-get install -y wget apt-transport-https software-properties-common
+
+# Get the version of Ubuntu
+source /etc/os-release
+
 # Download the Microsoft repository GPG keys
-wget -q https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb
+wget -q https://packages.microsoft.com/config/ubuntu/$VERSION_ID/packages-microsoft-prod.deb
 
 # Register the Microsoft repository GPG keys
 sudo dpkg -i packages-microsoft-prod.deb
+
+# Delete the Microsoft repository keys file
+rm packages-microsoft-prod.deb
 
 # Update the list of products
 sudo apt-get update
