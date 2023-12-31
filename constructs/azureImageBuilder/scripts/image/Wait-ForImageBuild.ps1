ca605882bf089fdff127bbe997462bd86f22edbc
AlexanderSehr <asehr@hotmail.de>
2023-12-31 13:58:27 +0100

    Update to latest

--- constructs/azureImageBuilder/scripts/image/Wait-ForImageBuild.ps1
+++ constructs/azureImageBuilder/scripts/image/Wait-ForImageBuild.ps1
@@ -55,12 +55,12 @@ function Wait-ForImageBuild {
 
         do {
             $latestStatus = Get-ImageTemplateStatus -templateResourceGroup $resourceGroupName -templateName $ImageTemplateName
-            $runState = $latestStatus.runState.ToLower()
-            if ($runState -notIn @('running', 'new')) {
 
-                if ($runState -eq 'failed') {
-                    throw $runState
-                }
+            if ($latestStatus -eq 'failed') {
+                throw "Image Template [$ImageTemplateName] build failed with status [failed]"
+            }
+
+            if ($latestStatus.runState.ToLower() -notIn @('running', 'new')) {
                 break
             }
 
